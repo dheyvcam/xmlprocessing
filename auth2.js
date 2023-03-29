8fe45a305b98552c4dc46241bfe1b24b9d69d32d
dheyvcam <128312175+dheyvcam@users.noreply.github.com>
2023-03-30 00:42:09 +0800

    Update auth2.js

--- auth2.js
+++ auth2.js
@@ -4,7 +4,7 @@ document.getElementById('login-form').addEventListener('submit', function(event)
       var username = document.getElementById('username').value;
       var password = document.getElementById('password').value;
 
-      if (username === 'xmladmin' && password === 'elevate2033') {
+      if (username === 'xmladmin' && password === 'elevate2023') {
         sessionStorage.setItem('loggedIn', 'true');
         window.location.href = 'index.html'; // Redirect to your main page
       } else {
