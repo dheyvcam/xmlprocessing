f419832d6cc6aef96b838e4d7071c2a3eab3178b
dheyvcam <128312175+dheyvcam@users.noreply.github.com>
2023-03-29 21:32:33 +0800

    Update auth2.js

--- auth2.js
+++ auth2.js
@@ -12,14 +12,4 @@ document.getElementById('login-form').addEventListener('submit', function(event)
       }
     });
 
-<!-- http://www.spacegun.co.uk -->
-
-var message = "function disabled";
-
-function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
-
-if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
-
-document.onmousedown = rtclickcheck;
-
 
