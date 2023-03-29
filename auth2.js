82e4a3db6808af7dc6c8be90df2e48b106408410
dheyvcam <128312175+dheyvcam@users.noreply.github.com>
2023-03-29 16:59:05 +0800

    Update auth2.js

--- auth2.js
+++ auth2.js
@@ -1,22 +1,16 @@
-function validate()
-{
+document.getElementById('login-form').addEventListener('submit', function(event) {
+      event.preventDefault();
 
       var username = document.getElementById('username').value;
       var password = document.getElementById('password').value;
 
       if (username === 'xmladmin' && password === 'elevate2023') {
-        alert ("login successfully");
+        sessionStorage.setItem('loggedIn', 'true');
         window.location.href = 'index.html'; // Redirect to your main page
-            return false;
-      } 
-      
-      else {
-        alert("Invalid username or password.");
+      } else {
+        alert('Invalid username or password.');
       }
-
-}
-
-
+    });
 
 
 
@@ -29,14 +23,3 @@ function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which =
 if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(message); return false; } }
 
 document.onmousedown = rtclickcheck;
-
- /*function check(e)
-    {
-    alert(e.keyCode);
-    }*/
-    document.onkeydown = function(e) {
-            if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {//Alt+c, Alt+v will also be disabled sadly.
-                alert('not allowed');
-            }
-            return false;
-    };
