44dff6ea9ffa17d51f5475f1197a2441d48965ca
dheyvcam <128312175+dheyvcam@users.noreply.github.com>
2023-03-29 21:30:37 +0800

    Update auth2.js

--- auth2.js
+++ auth2.js
@@ -22,14 +22,4 @@ if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { alert(mes
 
 document.onmousedown = rtclickcheck;
 
- /*function check(e)
-    {
-    alert(e.keyCode);
-    }*/
-    document.onkeydown = function(e) {
-            if (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86 || e.keyCode === 85 || e.keyCode === 117)) {//Alt+c, Alt+v will also be disabled sadly.
-                alert('not allowed');
-            }
-            return false;
-    };
 
